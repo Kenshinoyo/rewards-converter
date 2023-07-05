2c6d0f5e987be1e0db18943d97d99d4cd2da0a66
Garrad Belle <87483782+Kenshinoyo@users.noreply.github.com>
2023-07-04 23:32:00 -0400

    Creating constructor...

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,26 @@
+//- Con
+public class RewardValue extends RewardsConverter { /*<-- Make RewardValue a child class of the RewardsConverter,
+                                                            to allow access to already declared methods/values. */
+
+    //- Instance variables
+    double uCash; //<-- Separate variable for user's cash value
+    int uMiles; //<-- Separate variable for user's miles
+
+    //- Cash constructor(class type)
+    public RewardValue(double cashValue)
+    {
+        uCash = cashValue;
+    }
+
+    //- Miles constructor(class type)
+    public RewardValue()
+    {
+        uMiles =
+    }
+
+
+
+
+
+
+}
